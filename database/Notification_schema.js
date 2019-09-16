48560aa08235bf3946830425f1325e067689feb1
flyc4 <37125974+flyc4@users.noreply.github.com>
2019-09-16 16:44:27 +0900

    crawling 반영

---
+++ database/Notification_schema.js
@@ -0,0 +1,43 @@
+/**
+ *  공지사항에 대한 스키마
+ * 
+ */ 
+
+var utils = require('../config/utils')
+
+var SchemaObj = {};
+SchemaObj.createSchema = function(mongoose) {
+	
+	// 공지사항 스키마 정의
+	var NotificationSchema = mongoose.Schema({ 
+    
+            userid: {type: mongoose.Schema.ObjectId, ref: 'users'}, 
+            nickNm: {type: String, 'default': 'noName'},
+            profile: {type: String, trim:true, 'default': ' '},// 게시글 옆 사진
+            likes: {type: Number, unique: false, 'default': 0},
+            created_at: {type: Date, 'default': utils.timestamp(), index: {unique: false}}, //크롤링한 날짜
+            title: {type: String, trim:true, 'default': ' '},
+            contents: {type: String, trim:true, 'default': ' '},
+            pictures: {type: String, trim:true, 'default': ' '},  //링크
+            hits: {type: Number, unique: false, 'default': 0}, // 조회 수     
+            url: {type: String, trim:true, 'default': ' '}, 
+            date: {type: Date, 'default': utils.timestamp()}, //게시글 작성일. '4시간 전' 이런 식으로 작성되어있으면 현재 날짜로 
+            comments: [{   // 댓글           
+                userid: {type: mongoose.Schema.ObjectId, ref: 'users'},  
+                nickNm: {type: String, 'default': 'noName'},
+                boardid: {type: String, trim:true, 'default': ' '}, 
+                parentreplyid: {type: mongoose.Schema.ObjectId, 'default': ' ' }, //부모 댓글의 id
+                rootreplyid: {type: mongoose.Schema.ObjectId, 'default': ' ' }, //루트 댓글의 id
+                likes: {type: Number, unique: false, 'default': 0},
+                contents: {type: String, trim:true, 'default': ' '},
+                pictures: {type: String, trim:true, 'default': ' '}, 
+                created_at: {type: Date, 'default': utils.timestamp()}, 
+            }]
+    });
+    console.log('NotificationSchema 정의함.');
+	return NotificationSchema;
+};
+
+// module.exports에 NotificationSchema 객체 직접 할당
+module.exports = SchemaObj;
+
