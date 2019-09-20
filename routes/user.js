3f5ade3a7ab407727c1b232ad13ce8d3ceec3482
flyc4 <37125974+flyc4@users.noreply.github.com>
2019-09-20 10:09:27 +0900

    DM 보내기 및 사용자 이름 조회 완료

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,10 @@ var Entities = require('html-entities').AllHtmlEntities;
 
 var mongoose = require('mongoose');
 var expressSession = require('express-session');
-var jwt = require('jsonwebtoken');
+var jwt = require('jsonwebtoken'); 
+
+var ObjectId = mongoose.Types.ObjectId;  
+var utils = require('../config/utils');
 
 var permittednickNm = {permittednickNm: false}
 
@@ -96,5 +99,122 @@ var getuserid = function(req, res) {
         }   
 };//getuserid 닫기
 
+var SendDM = function(req, res) {
+    console.log('user 모듈 안에 있는 SendDM 호출됨.');
+  
+    var database = req.app.get('database'); 
+
+    var paramReceiverName = req.body.receivername||"no name"; 
+    var paramSenderId = req.body.senderid|| "000000000000000000000001"
+    var paramTitle = req.body.title||"no title";
+    var paramContents = req.body.contents||"no contents";
+
+    var context = {msg: " "}
+
+    console.log("paramSenderId: ", paramSenderId) 
+    console.log("paramREceiverName: ", paramReceiverName)  
+    console.log("paramTitle: ",paramTitle) 
+    console.log("paramContents: ",paramContents)
+    
+    // 데이터베이스 객체가 초기화된 경우
+	if (database.db) {
+		 	     
+        database.UserModel.findOne({_id: paramSenderId}, function(err1, sender) {
+            if (err1) {
+                utils.log("SendDM에서 송신자 조회 중 에러발생: ",err1.message)
+                context.msg = "missing" 
+                res.json(context)     
+                res.end();
+                return;
+            } 
+            else{  
+                
+                database.db.collection("users").updateOne({nickNm: paramReceiverName}, 
+                    {"$push": { 
+                        DM: {   
+                            sendername: sender.nickNm,
+                            senderid: sender._id,
+                            title: paramTitle,
+                            contents: paramContents, 
+                            created_at: utils.timestamp()
+                        }}
+                    },
+                    function(err, receiver) {		
+                    if (err) {
+                        utils.log("SendDM에서 수신자 조회 중 에러발생: ",err.message)
+                        context.msg = "missing" 
+                        res.json(context)     
+                        res.end();
+                        return;
+                    }
+                    if(!receiver){
+                        utils.log("SendDM에서 수신자 조회 실패")
+                        context.msg = "missing" 
+                        res.json(context)     
+                        res.end();
+                        return;  
+                    }
+                    context.msg = "success" 
+                    res.json(context) 
+                    res.end() 
+                    return;  
+                })//receiver 조회 닫기 
+            }//else 닫기
+        })// sender 조회 닫기  
+
+    } else {
+        utils.log('sendDM 수행 중 데이터베이스 연결 실패');
+        res.end(); 
+        return;
+        }   
+};//sendDM 닫기
+
+var ShowUserNameList = function(req, res) {
+    console.log('user 모듈 안에 있는 ShowUserNameList 호출됨.');
+  
+    var database = req.app.get('database'); 
+    var paramSearch = req.body.search||" ";
+    var context = {msg: " ", usernamelist: [{username: ' '}]} 
+    
+    // 데이터베이스 객체가 초기화된 경우
+	if (database.db) {
+        
+        if(paramSearch !=" "){
+            var query = {  
+                nickNm: new RegExp(".*"+paramSearch+".*","gi")
+              }; 
+          } 
+          else{
+            var query = {};
+          }
+
+        database.UserModel.find(query, function(err, users) {
+            if (err) {
+                utils.log("ShowUserNameLis에서 사용자 조회 중 에러발생: ",err.message)
+                context.msg = "missing" 
+                res.json(context)     
+                res.end();
+                return;
+            } 
+            else{  
+                context.msg = "succeess"; 
+                users.map((items)=>{
+                    context.usernamelist.push({username: items.nickNm})
+                }) 
+                context.usernamelist.splice(0,1) 
+                res.json(context) 
+                res.end() 
+                return 
+            } 
+        })//find 닫기
+    } else {
+        utils.log('ShowUserNameList 수행 중 데이터베이스 연결 실패');
+        res.end(); 
+        return;
+        }   
+};//ShowUserNameList 닫기
+
 module.exports.checknickNm = checknickNm;
 module.exports.getuserid = getuserid;
+module.exports.SendDM = SendDM; 
+module.exports.ShowUserNameList = ShowUserNameList;
